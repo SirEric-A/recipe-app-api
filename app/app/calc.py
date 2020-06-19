c92e809f9be161ca97029c5b7b1c23fe1053932d
Eric <snow__wons@163.com>
2020-06-19 12:07:15 +0800

    following flake8 rules

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,9 @@
-def add(x,y):
+def add(x, y):
     """add two numbers together"""
     return x+y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract y form  x and  retunr value"""
     return  x-y
+    
